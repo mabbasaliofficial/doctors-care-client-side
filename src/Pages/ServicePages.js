e019a53f0acfcebef7f0e71b3aa04265394af027
mabbasaliofficial <mabbasaliofficial@gmail.com>
2022-11-11 20:38:26 +0600

    all done

--- src/Pages/ServicePages.js
+++ src/Pages/ServicePages.js
@@ -9,10 +9,13 @@ const ServicePages = () => {
   const treatments = useLoaderData();
   console.log(typeof treatments);
   return (
-    <div className="mx-24 grid gap-12 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-10">
+    <div>
+      <h1 className="text-4xl font-extrabold text-center">All Services</h1>
+      <div className="mx-24 grid gap-12 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-10 mb-10">
       {treatments.map((treatment) => (
         <TreatmentCard key={treatment._id} treatment={treatment}></TreatmentCard>
       ))}
+      </div>
     </div>
   );
 };
